fee204853aad618f3758904569ce2fe859efaed7
Aaron Berns <anberns@gmail.com>
2019-01-27 15:02:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello World!"
+  end
+  
+  get '/name' do 
+    "My name is Aaron."
+  end
+  
+  get ('/hometown') { "My hometown is Wake Forest." }
+  
+  get ('/favorite-song') { "My favorite song is 'Hold Me'." }
 end
