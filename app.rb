69bf5e025b3b3b498bc104ac7e18e2d149889cde
Edgar Irizarry <eirizarry15@yahoo.com>
2019-03-05 15:06:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Edgar."
+  end
+
+  get '/hometown' do
+    "My hometown is Petaluma."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is New York New York."
+  end
+
 end
