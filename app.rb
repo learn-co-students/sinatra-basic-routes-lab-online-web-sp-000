bca82424e67c64c6a226d99caa6a638ca758552d
Arthur Song <arth3rs0ng@gmail.com>
2019-05-13 07:42:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Arthur"
+  end
+  
+  get '/hometown' do
+    "My hometown is Pasadena, California"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Sunflower by Rex Orange County"
+  end
 end
