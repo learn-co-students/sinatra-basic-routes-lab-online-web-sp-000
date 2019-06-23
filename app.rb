c19988306d71a799ec5ee948d946ddded932194f
Yingqi Chen <chanwk18@hotmail.com>
2019-06-23 00:44:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is KK"
+  end
+
+  get '/hometown' do
+    "My hometown is Hongkong"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is imagine"
+  end
+
+
 end
