b0c776c6d73a9cb774fcc9c7c1361ce093018071
Donna Amos <donnacamos.developer@gmail.com>
2019-07-02 03:01:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+   "Hello, World!"
+  end 
+  
+  get '/name'
+    "My name is Donna."
+  end
+  
+  get '/hometown'
+    "My hometown is Albany."
+  end 
+  
+  get '/favorite-song'
+    "My favorite song is You Are Holding Me."
+  end
 end
