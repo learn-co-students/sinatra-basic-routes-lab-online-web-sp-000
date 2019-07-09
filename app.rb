6d53bbc1cbdc1777ec0cd0c01be46b42336d0872
Ramon Chavez <rachavez1974@gmail.com>
2019-07-08 22:39:05 -0400

    Finish inatra-basic-routes-lab-online-web-sp-000

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Razor Ramon"
+  end
+
+  get '/hometown' do
+    "My hometown is Quaquanlanken"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Little Bitty Bar."
+  end
+
 end
