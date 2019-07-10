bd0881fc271ff42c133402acfa55401f677498ef
Alan Krajina <alankrajina88@gmail.com>
2019-07-10 12:55:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+get '/name' do
+  "My name is Alan"
+end
+
+get '/hometown' do
+  "My hometown is Zagreb"
+end
+
+get '/favorite-song' do
+  "My favorite song is Hi"
+end
+
 end
