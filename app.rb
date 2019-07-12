e3795c668553899e4415e18038550f5b436e196c
elijahmm17 <elijahmm17@gmail.com>
2019-07-12 04:43:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+    "Hello, World!" 
+  end 
+  
+  get '/name' do 
+    "My name is Eli"
+  end 
+  
+  get '/hometown' do 
+    "My hometown is Yorktown, Virginia"
+  end 
+  
+  get '/favorite-song' do 
+    "My favorite song is Ophelia by The Lumineers"
+  end 
 end
