a51a2f28f5ed5c8bfd2f5b733c4be94b659c9ff8
Jonathan Wong <jonathan.wong110@gmail.com>
2019-07-14 16:47:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Alex"
+  end
+  
+  get '/hometown' do
+    "My hometown is New York City"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is River Flows in You"
+  end
+  
 end
