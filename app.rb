954f465f70d003ee1c2d59177860b3d2eb92b2f5
Abagail Hudson <abagailanne@gmail.com>
2019-07-22 22:46:08 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Abagail."
+  end
+
+  get '/hometown' do
+    "My hometown is New York."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Hey' by The Pixies"
+  end
 end
