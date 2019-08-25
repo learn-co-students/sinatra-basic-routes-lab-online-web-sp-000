4dd3be79e2b7ae3ddec2de95c07fe159e450dc46
Peter Grant <jgrantps@hotmail.com>
2019-08-25 16:11:28 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Peter"
+  end
+
+  get '/hometown' do
+    "My hometown is Rocky"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Rachmaninoff Preludes"
+  end
 end
