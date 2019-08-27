dd61795cb6f7b0ab23db541e16f1c4830026a26e
Stuart Hahn <github email address>
2019-08-27 15:11:55 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Stuart"
+    end
+
+    get '/hometown' do
+        "My hometown is everywhere."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Dance Anthem of the 80's."
+    end
+
 end
