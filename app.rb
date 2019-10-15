4d3f172a54cfbc91dceb44a280554edb8735435f
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-10-15 04:41:15 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Mark."
+  end
+  
+  get '/hometown' do
+    "My hometown is currently Edgewood."
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is what ever makes me wanna dance in the moment."
+  end
+  
 end
