ff8463425db952202c48a11772ed1abb0cf810d8
thisisSDK <25039980+thisisSDK@users.noreply.github.com>
2019-10-15 15:41:33 -0400

    creates homepage, name, hometown, and favorite-song routes

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Slim Shady"
+  end
+
+  get '/hometown' do
+    "My hometown is Detroit"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is The Real Slim Shady by Eminem"
+  end
 end
