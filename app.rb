5da1f36a21cc52bb71cd5f6a56c4b01c1e248482
BlythePollard <github email address>
2019-11-30 12:02:02 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Blythe"
+  end
+
+  get '/hometown' do
+    "My hometown is Denver"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Justin Beiber"
+  end
 end
