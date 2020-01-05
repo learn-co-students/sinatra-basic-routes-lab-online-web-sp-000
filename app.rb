3ff4054effa5fd1920fc0748ba279edd60dddb64
RickLearnsToCode <rick.rosetti@gmail.com>
2020-01-05 12:04:24 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do
+    "My name is Rick"
+end
+
+get '/hometown' do
+    "My hometown is Flossmoor, Il"
+end
+
+get '/favorite-song' do
+    "My favorite song is 'Killing in the Name of...' "
+end
+
 end
