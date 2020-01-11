215d1e503915b6ef43b544b43a6541c355d893df
Garrett Thompson <gtcruz.gt@gmail.com>
2020-01-11 03:18:58 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Garrett."
+  end
+
+  get '/hometown' do
+    "My hometown is Roseville."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is House of the Raising Sun."
+  end
 end
