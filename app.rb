88ddafe74c452bc8bd63fcee59cf0d75af7f3417
Alex Velasquez <jtinnesco@gmail.com>
2020-02-06 15:02:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Alex"
+  end
+
+  get '/hometown' do
+    "My hometown is Sacramento. California"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Sicko World by Travis Scott"
+  end
 end
