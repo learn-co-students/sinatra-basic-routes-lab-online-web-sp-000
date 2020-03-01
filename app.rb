d31b5fd67c471ef2ef2a680d48100915087cdd44
ottocedeno <otto@creativespaceclub.com>
2020-02-29 21:26:29 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hell World!"
+  end
+
+  get '/name' do
+    "My name is Otto"
+  end
+
+  get '/hometown' do
+    "My hometown is Westminster, CA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Master of Puppets"
+  end
 end
