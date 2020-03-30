73cbf73cabecbee103c2157fa213b051ce134d0b
Chad Lengefeld <chadlengefeld@gmail.com>
2020-03-30 07:43:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+      get '/name' do
+      "My name is Chad"
+      end
+
+
+  get '/hometown' do
+    "My hometown is Round Rock, Texas"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Lateralus by Tool"
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,7 @@ ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
+require 'sinatra'
 
 require './app'
 require_all 'models'
