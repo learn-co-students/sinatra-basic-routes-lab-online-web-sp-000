4310207d85845c67318bf7fe195132c9151d0c97
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-04-21 01:36:35 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/' do
+		"Hello, World!"
+	end
+
+	get '/name' do
+		"My name is Shaquille"
+	end
+
+	get '/hometown' do
+		"My hometown is Cavite, Philippines"
+	end
+
+	get '/favorite-song' do
+		"My favorite song is Araw-araw"
+	end
 end
