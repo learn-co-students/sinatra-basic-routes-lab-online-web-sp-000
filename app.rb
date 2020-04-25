05f81261a72c6bb5fe1dadfb8f830c5fd0f559e9
Brian Vehige <brianvehige@gmail.com>
2020-04-25 10:54:54 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Rocky Balboa"
+    end
+
+    get '/hometown' do
+        "My hometown is Philadelphia"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Eye Of The Tiger"
+    end
+    
 end
