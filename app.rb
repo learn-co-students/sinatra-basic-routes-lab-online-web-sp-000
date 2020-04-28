333ac9d56a9f744d7e9c186fb2306a9d65aa8486
Alvin Coulson <aaacoulson@gmail.com>
2020-04-28 11:35:24 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Alvin"
+    end
+
+    get '/hometown' do
+        "My hometown is Freetown"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Just the way you are by Barry White"
+    end
 end
