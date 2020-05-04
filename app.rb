96586a90777b4cf05d761109e6578027f7d14e8b
dayofgray <graysonpberger@gmail.com>
2020-05-04 19:06:32 -0400

    Revert "Done."
    This reverts commit 76fd1a62b099e7452ec0aefb7a93727a53a6921c.

--- app.rb
+++ app.rb
@@ -1,20 +1,4 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
-    get '/' do
-        "Hello, World!"
-    end
-
-    get '/name' do
-        "My name is Grayson"
-    end
-
-    get '/hometown' do
-        "My hometown is Atlanta"
-    end
-
-    get '/favorite-song' do
-        "My favorite song is I Choose You"
-    end
 end
