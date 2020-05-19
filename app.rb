93e123228131e3a20bb61600bb8c21b2a3e50fa9
bibraj96 <blendi.ibraj96@gmail.com>
2020-05-19 15:17:53 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Blend"
+  end
+
+  get '/hometown' do
+    "My hometown is Manhattan, New York"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'All is Full of Love' by Bjork"
+  end
+
 end
