21f89dbbd3e7e0d24e3e6b6ae60689a87e70d64d
clincoln12 <chris.lincoln12@gmail.com>
2020-06-18 19:29:12 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Chris Lincoln"
+  end
+
+  get '/hometown' do
+    "My hometown is Marietta, GA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Blinding Lights - The Weeknd"
+  end
 end
