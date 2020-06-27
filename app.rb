9f8c8538a9a68b8e8451939b6fef99ba0ee831e2
AustinRhoads <austinrrhoads@ymail.com>
2020-06-27 14:27:52 -0500

    all tests pass

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+      end
+
+      get '/name' do
+        "My name is Aush."
+      end
+      get '/hometown' do
+        "My hometown is Galveston, TX."
+      end
+      get '/favorite-song' do
+        "My favorite song is Love & mercy."
+      end
 end
