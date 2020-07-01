0b44eb3438b4feb305217512a245eb3983dfdf5a
tbanish <tombanish@gmail.com>
2020-06-30 22:18:36 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Tom"
+  end
+
+  get '/hometown' do
+    "My hometown is Elmont"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Little Martha"
+  end
 end
