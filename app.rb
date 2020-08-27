68c459c4c5ef60b35cdefcfab22689031c178bc4
Erin Stansbury <ekstansbury@gmail.com>
2020-08-27 03:04:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Erin"
+  end
+  
+  get '/hometown' do
+    "My hometown is htown"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Motion Sickness"
+  end
 end
