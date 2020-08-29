9c2b97d60505cbf132ddfd882231f2c1dc55e322
ismayton <ian.mayton@gmail.com>
2020-08-28 19:24:43 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do 
+        "Welcome to the homepage!"
+    end 
+
+    get '/name' do
+        "My name is Ian."
+    end 
+
+    get '/hometown' do 
+        "My hometown is Durham, NC."
+    end 
+
+    get '/favorite-song' do 
+        "My favorite song is ... How could I possibly pick just one??"
+    end 
 end
