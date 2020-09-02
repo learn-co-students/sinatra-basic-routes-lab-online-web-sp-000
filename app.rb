683d99fb8c30b7a75225a75c0fe81411d5c9b625
carriemathieu <carrieburks@gmail.com>
2020-09-02 18:04:29 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do 
+        "My name is Carrie"
+    end
+
+    get '/hometown' do 
+        "My hometown is in Georgia"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is Tout Seule"
+    end
 end
