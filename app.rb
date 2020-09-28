86a841eb7ed435aa699e1bfb6ea43388935d86b9
Alexander M. Clamor <alexander.clamor@gmail.com>
2020-09-28 14:53:15 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Alexander"
+    end
+
+    get '/hometown' do
+        "My hometown is Melbourne"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is One Day by Matisyahu"
+    end
+
 end
