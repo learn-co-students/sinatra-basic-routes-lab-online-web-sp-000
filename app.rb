9087e366348965ec5442cffda63e89157fcdac4c
jeanettepayne <jeanetterpayne@gmail.com>
2020-10-18 15:18:23 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Jeanette."
+    end
+
+    get '/hometown' do
+        "My hometown is Mt. Vernon, Illinois."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'Something Beautiful' by Needtobreathe."
+    end
+
+
+
 end
