0b9385f4c8c88f8e0128564a67fe7493288218e6
Harley Hanna <harleyhannawrit@gmail.com>
2020-10-22 14:17:08 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do 
+    "My name is Harley"
+  end
+  
+  get '/hometown' do
+    "My hometown is Erie"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Edgelord"
+  end
+  
+  
 end
