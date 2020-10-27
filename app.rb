ddd64f3c941e7a33ea269a657d3fa283fbbf450c
Alie Stone <helloaliestone@gmail.com>
2020-10-27 15:31:53 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do
+  "My name is Alie"
+end
+
+get '/hometown' do
+  "My hometown is Los Angeles"
+end
+
+get '/favorite-song' do
+  "My favorite song is fields of gold"
+end
+
 end
