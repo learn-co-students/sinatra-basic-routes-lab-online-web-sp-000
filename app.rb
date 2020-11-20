2004e06b4ec38bb5191b0fc61e6cf0dcdef57d88
JustinBreneman <Justin.Breneman@yahoo.com>
2020-11-19 18:05:31 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Justin Breneman."
+    end
+
+    get '/hometown' do
+        "My hometown is Hubbard OR."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Overkill by Riot."
+    end
 end
