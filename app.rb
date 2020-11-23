41c10c73e739df50abf0c5a44c51eedf67395db6
Chandra Lama <iamme24cl@gmail.com>
2020-11-22 22:42:23 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get "/" do 
+        "Hello, World!"
+    end
+
+    get "/name" do
+        "My name is Chandra Lama."
+    end
+
+    get "/hometown" do
+        "My hometown is Boudha."
+    end
+
+    get "/favorite-song" do
+        "My favorite song is i don't know!"
+    end
 end
