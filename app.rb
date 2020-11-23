8e95f912205da5c090e3b99aa1df6aa981c2b229
Ryan Burns <ryankburns@gmail.com>
2020-11-23 12:51:30 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Ryan"
+    end
+
+    get '/hometown' do
+        "My hometown is Sacramento"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Usher-Daddys Home"
+    end
+
 end
