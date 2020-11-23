4b44974cea855a1f6498e24d43cf131c5002fa90
catprice444 <catprice444@gmail.com>
2020-11-23 16:26:48 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Cat."
+    end 
+
+    get '/hometown' do
+        "My hometown is Pittsburgh."
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is Southside Serb."
+    end 
 end
