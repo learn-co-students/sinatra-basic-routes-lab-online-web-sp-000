4f2ce034b31ec12d234c8013e6c25b21de23f198
Anton-Kalyutich <anton.kalyutich@gmail.com>
2020-12-05 09:19:40 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get "/name" do
+        "My name is Anton"
+    end
+
+    get "/hometown" do
+        "My hometown is Michurinsk"
+    end
+
+    get "/favorite-song" do
+        "My favorite song is 'Ocean Drive'"
+    end
 end
