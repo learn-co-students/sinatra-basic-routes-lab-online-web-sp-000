40c939cdb6670e7f7b9a324bfc9f60c1b688fde3
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-19 01:32:17 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get "/name" do
+    "My name is James"
+  end
+
+  get "/hometown" do
+    "My hometown is Sunbury"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Mein Herz Brennt"
+  end
 end
