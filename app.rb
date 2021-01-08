3f76d4148f41484c534fe146c87f507e66f92233
Jose Melero Martinez <jmelero08@gmail.com>
2021-01-08 01:26:43 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do 
+    "My name is Jose"
+  end
+  
+  get '/hometown' do 
+    "My hometown is Phoenix"
+  end
+
+  get '/favorite-song' do
+    " My favorite song is This Is The House That Doubt Built"
+  end
+  
 end
