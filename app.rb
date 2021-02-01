c5187627f85e2e814f3bde1acc7830c71528aed1
Alison Williams <apw5011@gmail.com>
2021-02-01 15:32:28 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Alison."
+    end 
+
+    get '/hometown' do
+        "My hometown is Bethlehem."
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is Perfect."
+    end
 end
