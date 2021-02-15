7ea65e2c9bc2c24dbb0639353fff22b4659c1615
A.J. Romaniello <ajiellodev@gmail.com>
2021-02-15 16:35:40 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    'My name is A.J.'
+  end
+
+  get '/hometown' do
+    'My hometown is Atlanta, GA'
+  end
+
+  get '/favorite-song'do
+    'My favorite song is Dreams by Fleetwood Mac'
+  end
 end
