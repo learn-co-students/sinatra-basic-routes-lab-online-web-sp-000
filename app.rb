404a099a60610331cd70ac83dbb58879ff1fd4ea
dbrandt1990 <dvbrandt90@gmail.com>
2021-02-22 09:19:52 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+      end
+
+    get '/name' do
+      "My name is Danny"
+    end
+
+    get '/hometown' do
+      "My hometown is Ocala Florida"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Falling in Love With You"
+      end
 end
