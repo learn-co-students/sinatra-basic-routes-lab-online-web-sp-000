1ae7686632650e321baee5200010b96a150af5eb
Tres Tarwater <trest03@gmail.com>
2021-03-02 06:27:47 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Jessie."
+    end
+
+    get '/hometown' do
+        "My hometown is Elizabethtown."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is The Sign by Ace of Base."
+    end
 end
