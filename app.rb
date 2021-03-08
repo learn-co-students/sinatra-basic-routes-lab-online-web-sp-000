b8d88ee6d90c8a0bced625475c6bef37c1993051
Claire Bullock <bullockcm13@gmail.com>
2021-03-08 20:54:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Me."
+  end
+    
+  get '/hometown' do 
+    "My hometown is here."
+  end 
+    
+  get '/favorite-song' do 
+    "My favorite song is Song."
+  end 
 end
