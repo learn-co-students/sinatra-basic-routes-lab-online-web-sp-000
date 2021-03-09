99a0f1f5165c200c23268d60c941bd5840af6596
Kiana Long <kianajade42.kl@gmail.com>
2021-03-09 18:43:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+  "My name is Kiana"
+end
+get '/hometown' do
+  "My hometown is Michigan"
+end
+get '/favorite-song' do
+"My favorite song is impossible to choose"
+end
 end
