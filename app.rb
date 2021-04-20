37086714d6d514eaaae440ec6b764768b803af54
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-20 15:39:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Kelly"
+end
+
+  get '/hometown' do
+    "My hometown is Alpine"
+end
+
+get '/favorite-song' do
+  "My favorite song is Baby Shark"
+end
+
 end
