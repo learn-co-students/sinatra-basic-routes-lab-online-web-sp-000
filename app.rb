ca8914f1ee178249aaef547d11ccc9916e5b20c1
Chris Baum <Christopher.Baum@valpo.edu>
2021-04-21 21:21:07 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Chris Baum."
+  end
+
+  get '/hometown' do
+    "My hometown is Fort Mill, SC."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Tribute."
+  end
 end
