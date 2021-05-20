49c26c36a62aeb0b7bfb3a2470ea2213a2efdcfd
Janis Lebron <janislebron@gmail.com>
2021-05-20 00:08:49 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+  "Hello, World!"
+  end
+  
+  get '/name' do
+  "My name is Janis"
+  end
+  
+  get '/hometown' do
+  "My hometown is the Bronx"
+  end
+  
+  get '/favorite-song' do
+  "My favorite song is Black Parade"
+  end
+  
 end
