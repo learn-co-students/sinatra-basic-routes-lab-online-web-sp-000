957985f8ca4f10c0d10f3d5bdae344145da62c14
VandelayUtd <drewhadley.guitar@gmail.com>
2021-05-25 08:43:01 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,35 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
+class App < Sinatra::Base 
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Drew"
+    end
+
+    get '/hometown' do
+        "My hometown is Charleston"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Wonderwall"
+    end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
