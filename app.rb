aba41a580a67bba387be3b82e56aa3c9f0295b26
Alla Kuzminova <alchik@gmail.com>
2021-05-30 18:05:10 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Alla"
+    end
+    get '/hometown' do
+        "My hometown is New York"
+    end
+    get '/favorite-song' do
+        "My favorite song is New York, New York"
+    end
 end
